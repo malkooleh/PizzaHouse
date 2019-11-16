5141002663e4c9b7c71bd5758e64e5814f47bc8e
malkooleh <07091991maluy>
2019-11-16 12:20:28 +0200

    discoveryService client

---
+++ web-app/src/main/java/ua/pizzeria/controller/LicenseServiceController.java
@@ -0,0 +1,32 @@
+package ua.pizzeria.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ua.pizzeria.controller.dto.License;
+import ua.pizzeria.services.LicenseService;
+
+@RestController
+@RequestMapping(value = "v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+    @Autowired
+    private LicenseService licenseService;
+
+    @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
+    public License getLicenses(@PathVariable("organizationId") String organizationId,
+                               @PathVariable("licenseId") String licenseId) {
+
+        return licenseService.getLicense(organizationId, licenseId, "");
+    }
+
+    @RequestMapping(value = "/{licenseId}/{clientType}", method = RequestMethod.GET)
+    public License getLicensesWithClient(@PathVariable("organizationId") String organizationId,
+                                         @PathVariable("licenseId") String licenseId,
+                                         @PathVariable("clientType") String clientType) {
+
+        return licenseService.getLicense(organizationId, licenseId, clientType);
+    }
+
+}
