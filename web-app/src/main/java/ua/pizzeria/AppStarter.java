8afac0540c954a00d481872aa95e7b7ef5f8c5f5
malkooleh <07091991maluy>
2019-11-17 12:48:25 +0200

    start with Hystrix

--- web-app/src/main/java/ua/pizzeria/AppStarter.java
+++ web-app/src/main/java/ua/pizzeria/AppStarter.java
@@ -2,6 +2,7 @@ package ua.pizzeria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker // use Hystrix
 @EnableDiscoveryClient //activates the Spring DiscoveryClient for use
 @EnableFeignClients // is needed to use the FeignClient
 public class AppStarter {
