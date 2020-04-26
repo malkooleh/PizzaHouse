d7caccc260b051e08567c2cb8050222afb70a568
malkooleh <07091991maluy>
2020-04-26 15:04:12 +0300

    oauth2 tests

---
+++ web-app/src/main/java/ua/pizzeria/security/OAuth2ResourceServerConfig.java
@@ -0,0 +1,21 @@
+package ua.pizzeria.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@EnableResourceServer
+@Configuration
+// This isn't the main/standard Resource Server of the project (that's in a different module)
+// This is the Resource Server for the Testing OAuth2 with Spring MVC article: http://www.baeldung.com/oauth-api-testing-with-spring-mvc
+// Notice that it's only active via the mvc profile
+public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/items").hasRole("ADMIN");
+
+    }
+
+}
---
+++ web-app/src/test/java/ua/pizzeria/oauth/OAuthMvcTest.java
@@ -0,0 +1,104 @@
+package ua.pizzeria.oauth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+import ua.pizzeria.AppStarter;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@SpringBootTest(classes = AppStarter.class)
+@ActiveProfiles("mvc")
+public class OAuthMvcTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private FilterChainProxy springSecurityFilterChain;
+
+    private MockMvc mockMvc;
+
+    private static final String CLIENT_ID = "user";
+    private static final String CLIENT_SECRET = "thisissecret";
+
+    private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).addFilter(springSecurityFilterChain).build();
+    }
+
+    private String obtainAccessToken(String username, String password) throws Exception {
+        final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+//        params.add("client_id", CLIENT_ID);
+        params.add("scope", "webclient");
+        params.add("username", username);
+        params.add("password", password);
+
+        ResultActions result = mockMvc.perform(post("/oauth/token")
+                .params(params)
+                .with(httpBasic(CLIENT_ID, CLIENT_SECRET))
+                .accept(CONTENT_TYPE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(CONTENT_TYPE));
+
+        String resultString = result.andReturn().getResponse().getContentAsString();
+        log.debug("obtainAccessToken : {}", resultString);
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resultString).get("access_token").toString();
+    }
+
+    @Test
+    public void givenNoToken_whenGetSecureRequest_thenUnauthorized() throws Exception {
+        mockMvc.perform(get("/items")).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void givenInvalidRole_whenGetSecureRequest_thenForbidden() throws Exception {
+        final String accessToken = obtainAccessToken("user", "user_password");
+        log.info("token:" + accessToken);
+
+        mockMvc.perform(get("/items")
+                .header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void givenToken_whenPostGetSecureRequest_thenOk() throws Exception {
+        final String accessToken = obtainAccessToken("admin", "admin_password");
+
+        log.info("token:" + accessToken);
+
+        mockMvc.perform(get("/items")
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(CONTENT_TYPE)
+                .accept(CONTENT_TYPE))
+                .andExpect(status().isOk());
+    }
+
+}
